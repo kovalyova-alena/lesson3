fefdce0acd60b4b43c7a4f163b04c1c0b66bf64f
kovalyova-alena <kovalyova_alena@inbox.ru>
2016-10-27 15:07:41 +0300

    create functions addElement, removeElement, insertBefore

--- js/main.js
+++ js/main.js
@@ -1,42 +1,67 @@
 function selectFirst() {
 	var findId = document.getElementById('elements'),
 		firstChild = findId.firstElementChild;
-
-	
-	for (var i = 0; i < findId.children.length; i++) {
-  		findId.children[i].classList.remove('active'); 
-	}
-	
+	removeClassActive();
     firstChild.classList.add('active');
 }
 
 function selectLast() {
 	var findId = document.getElementById('elements'),
 		lastChild = findId.lastElementChild;
-		
-	for (var i = 0; i < findId.children.length; i++) {
-  		findId.children[i].classList.remove('active'); 
-	}
-		
+	removeClassActive();		
 	lastChild.classList.add('active');
 }
 
-function selectNext() {
-	alert('jkjkj');
+function removeClassActive() {
+	var findId = document.getElementById('elements');
+	for (var i = 0; i < findId.children.length; i++) {
+  		findId.children[i].classList.remove('active'); 
+	}
 }
+
+function selectNext() {
+	var findId = document.getElementById('elements');
+	var findLiActive = document.getElementsByClassName('active');
+	var findLi = document.getElementsByTagName('li');	
+ }
 
 function selectPrevious() {
 	alert('6576765');
 }
 
-function addElement() {
-	alert('mnmnmn');
+function addElement(form) {
+	var formValue = form.insert.value;
+	if (formValue != 0) {
+		var findId = document.getElementById('elements'),
+		newItem = document.createElement('li');
+		newItem.innerHTML = formValue;
+		removeClassActive();
+		findId.appendChild(newItem).classList.add('active');
+	} else {
+		alert("Введите значение");
+	}
 }
 
 function removeElement() {
-	alert('uyyyy');
+	var findId = document.getElementById('elements'),
+		lastItem = findId.lastElementChild;
+	if (lastItem != null) { 
+        findId.removeChild(lastItem);
+    } else {
+    	alert("Удалять больше нечего. Сначала добавьте элементы")
+    }		
 }
 
-function insertUp() {
-	alert('xccc');
+function insertUp(form) {
+	var formValue = form.insert.value;
+	if (formValue != 0) {
+	var findId = document.getElementById('elements'),	
+		newItem = document.createElement('li'),
+		newText = document.createTextNode(formValue);
+	newItem.appendChild(newText);
+	removeClassActive();
+	findId.insertBefore(newItem, findId.firstElementChild).classList.add('active');
+	} else {
+		alert("Введите значение");
+	}
 }
